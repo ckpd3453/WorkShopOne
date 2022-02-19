250b13e1bb78ba21d79623965246f77843266d6c
ckpd3453 <98898121+ckpd3453@users.noreply.github.com>
2022-02-19 15:24:53 +0530

    Added Modified UC-1 Programme

--- src/GamblerUc1.java
+++ src/GamblerUc1.java
@@ -5,34 +5,26 @@ public class GamblerUc1 {
 	public static void main(String[] args) {
 	int i=1;
 	int j=100;
-	//User Input
 	try (Scanner scanner = new Scanner(System.in)) {
-		System.out.println("Enter the amount that gambler bet : ");
-		int betting = scanner.nextInt();
-		System.out.println("Enter the no. of Days : ");
-		int Day = scanner.nextInt();
-		//Loop for calculating Total Winning or Loosing Amount
-		while(i<=Day && betting<=j)
+	System.out.println("Enter the no. of Days : ");
+	int Day = scanner.nextInt();
+	while( i<= Day)
+	{
+		int bet = (int) Math.floor((Math.random()*10)%2);
+		if (bet ==1)
 		{
-				int bet = (int) Math.floor((Math.random()*10)%2);
-				if (bet ==1)
-				{
-					j += betting;
-					System.out.println("Won");
-					System.out.println("Total Amount =" +j);
-					i++;
-				}
-				else
-				{
-					j -=betting;
-					System.out.println("Loose");
-					System.out.println("Total Amount =" +j);
-					i++;
-				}
-			}
+			j += 1;
+			System.out.println("Won");
+			System.out.println("Total Amount =" +j);		
 		}
-		
-
+		else
+		{
+			j -=1;
+			System.out.println("Loose");
+			System.out.println("Total Amount =" +j);	
+		}
+		i++;
 	}
-
+	}
+}
 }
